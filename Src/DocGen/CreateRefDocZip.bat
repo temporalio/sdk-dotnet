1eaab14350c2544ff1f888fe53be8d14cae56a82
macrogreg <macrogreg@live.com>
2022-06-03 08:28:20 -0700

    Fix file placement issue to correctly upload the Docs artifact.

--- Src/DocGen/CreateRefDocZip.bat
+++ Src/DocGen/CreateRefDocZip.bat
@@ -1,9 +1,13 @@
+SET PK_ZIPFILE_NAME=TemporalIO.GitHub.IO_Sdk-DotNet.zip
+
 SET PK_ORIGIN=%CD%
 
 CD %~dp0
 
-7z a -tzip -mx9 TemporalIO.GitHub.IO_Sdk-DotNet.zip temporalio.github.io\sdk-dotnet
-7z a -tzip -mx9 TemporalIO.GitHub.IO_Sdk-DotNet.zip ApiReference.ReadMe.md
-7z a -tzip -mx9 TemporalIO.GitHub.IO_Sdk-DotNet.zip HostDocs.bat
+7z a -tzip -mx9 %PK_ZIPFILE_NAME% temporalio.github.io\sdk-dotnet
+7z a -tzip -mx9 %PK_ZIPFILE_NAME% ApiReference.ReadMe.md
+7z a -tzip -mx9 %PK_ZIPFILE_NAME% HostDocs.bat
 
 CD %PK_ORIGIN%
+
+MOVE /Y "%~dp0%PK_ZIPFILE_NAME%" .
