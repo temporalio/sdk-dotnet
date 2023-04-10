c0d8d118a93bbc95eb612312d4fba04d03f13799
Chad Retz <chad@temporal.io>
2023-04-10 13:58:06 -0500

    Minor CI fix and minor options update

--- src/Temporalio/Client/TemporalClientConnectOptions.cs
+++ src/Temporalio/Client/TemporalClientConnectOptions.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Temporalio.Api.Enums.V1;
+
 namespace Temporalio.Client
 {
     /// <summary>
@@ -42,6 +47,26 @@ namespace Temporalio.Client
             Converters.DataConverter.Default;
 
         /// <summary>
+        /// Gets or sets the interceptors to intercept client calls.
+        /// </summary>
+        /// <remarks>
+        /// Earlier interceptors in the list wrap later ones. If the interceptor in the list also
+        /// implements <see cref="Worker.Interceptors.IWorkerInterceptor" />, it will automatically
+        /// be used when the worker is created.
+        /// </remarks>
+        public IEnumerable<Interceptors.IClientInterceptor>? Interceptors { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logging factory used by loggers in Temporal.
+        /// </summary>
+        public ILoggerFactory LoggerFactory { get; set; } = NullLoggerFactory.Instance;
+
+        /// <summary>
+        /// Gets or sets the query rejection condition. This can be overridden on a per-query basis.
+        /// </summary>
+        public QueryRejectCondition? QueryRejectCondition { get; set; }
+
+        /// <summary>
         /// Create client options from a subset of these options for use in
         /// <see cref="TemporalClient.TemporalClient" />.
         /// </summary>
@@ -51,6 +76,9 @@ namespace Temporalio.Client
             {
                 Namespace = Namespace,
                 DataConverter = DataConverter,
+                Interceptors = Interceptors,
+                LoggerFactory = LoggerFactory,
+                QueryRejectCondition = QueryRejectCondition,
             };
     }
 }
