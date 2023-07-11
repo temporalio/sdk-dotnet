80106f211dedd79eba3c991f334921a25424613d
Chad Retz <chad@temporal.io>
2023-07-11 10:29:48 -0500

    .NET framework copy targets and smoke test

---
+++ tests/Temporalio.SmokeTestDotNetFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Temporalio.Api.WorkflowService.V1;
+using Temporalio.Testing;
+
+namespace Temporalio.SmokeTestDotNetFramework
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            var env = await WorkflowEnvironment.StartLocalAsync();
+            try
+            {
+                Console.WriteLine(
+                    "System info: {0}",
+                    await env.Client.Connection.WorkflowService.GetSystemInfoAsync(new GetSystemInfoRequest()));
+            }
+            finally
+            {
+                await env.ShutdownAsync();
+            }
+        }
+    }
+}
