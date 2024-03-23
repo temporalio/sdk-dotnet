8f6c0d90a153f11e666bee9a5d33b410317ba96b
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2024-03-23 11:23:21 -0500

    Alternative CreateTemporalActivityDefinition
    Allow the usage of AsyncServiceProvider for NET6+.

--- src/Temporalio.Extensions.Hosting/ServiceProviderExtensions.cs
+++ src/Temporalio.Extensions.Hosting/ServiceProviderExtensions.cs
@@ -41,7 +41,11 @@ namespace Temporalio.Extensions.Hosting
             type.
                 GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance).
                 Where(method => method.IsDefined(typeof(ActivityAttribute))).
+#if NET6_0_OR_GREATER
+                Select(method => provider.AsyncCreateTemporalActivityDefinition(type, method)).
+#else
                 Select(method => provider.CreateTemporalActivityDefinition(type, method)).
+#endif
                 ToList();
 
         /// <summary>
@@ -58,12 +62,12 @@ namespace Temporalio.Extensions.Hosting
             this IServiceProvider provider, Type instanceType, MethodInfo method)
         {
             // Invoker can be async (i.e. returns Task<object?>)
-            Func<object?[], Task<object>> invoker = async args =>
+            Func<object?[], Task<object?>> invoker = async args =>
             {
                 // Wrap in a scope (even for statics to keep logic simple)
                 using (var scope = provider.CreateScope())
                 {
-                    object? result;
+                    object? result = null;
                     try
                     {
                         // Invoke static or non-static
@@ -97,5 +101,66 @@ namespace Temporalio.Extensions.Hosting
             };
             return ActivityDefinition.Create(method, invoker);
         }
+
+#if NET6_0_OR_GREATER
+        /// <summary>
+        /// Create activity definition for the given activity-attributed method on the given
+        /// instance type. If the method is non-static, this will use the service provider to get
+        /// the instance to call the method on.
+        /// </summary>
+        /// <param name="provider">Service provider for creating the instance if the method is
+        /// non-static.</param>
+        /// <param name="instanceType">Type of the instance.</param>
+        /// <param name="method">Method to create activity definition from.</param>
+        /// <returns>Created definition.</returns>
+        public static ActivityDefinition AsyncCreateTemporalActivityDefinition(
+            this IServiceProvider provider,
+            Type instanceType,
+            MethodInfo method)
+        {
+            // Invoker can be async (i.e. returns Task<object?>)
+            async Task<object?> Invoker(object?[] args)
+            {
+                // Wrap in a scope (even for statics to keep logic simple)
+#pragma warning disable CA2007 // Invalid for AsyncServiceScope, ServiceProvider is not accessible via ConfiguredAsyncDisposable object type.
+                await using var scope = provider.CreateAsyncScope();
+#pragma warning restore CA2007
+
+                object? result = null;
+                try
+                {
+                    // Invoke static or non-static
+                    var instance = method.IsStatic
+                        ? null
+                        : scope.ServiceProvider.GetRequiredService(instanceType);
+
+                    result = method.Invoke(instance, args);
+                }
+                catch (TargetInvocationException e)
+                {
+                    ExceptionDispatchInfo.Capture(e.InnerException!).Throw();
+                }
+
+                // In order to make sure the scope lasts the life of the activity, we need to
+                // wait on the task if it's a task
+                if (result is Task resultTask)
+                {
+                    await resultTask.ConfigureAwait(false);
+                    // We have to use reflection to extract value if it's a Task<>
+                    var resultTaskType = resultTask.GetType();
+                    if (resultTaskType.IsGenericType)
+                    {
+                        result = resultTaskType.GetProperty("Result")!.GetValue(resultTask);
+                    }
+                    else
+                    {
+                        result = ValueTuple.Create();
+                    }
+                }
+                return result!;
+            }
+            return ActivityDefinition.Create(method, Invoker);
+        }
+#endif
     }
 }
