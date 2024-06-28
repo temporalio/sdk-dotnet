e6a3ba50ae52be5a991232990bcb586e9a6e2542
Spencer Judge <spencer@temporal.io>
2024-06-27 17:10:02 -0700

    Missing docstring

--- src/Temporalio/Worker/Tuning/ISlotSupplier.cs
+++ src/Temporalio/Worker/Tuning/ISlotSupplier.cs
@@ -3,7 +3,11 @@
 namespace Temporalio.Worker.Tuning
 {
     /// <summary>
-    /// TODO.
+    /// Slot suppliers control how slots are handed out for workflow and activity tasks as well as
+    /// local activities when used in conjunction with a <see cref="WorkerTuner"/>.
+    ///
+    /// Currently you cannot implement your own slot supplier, but you can use the provided <see
+    /// cref="FixedSizeSlotSupplier"/> and <see cref="ResourceBasedSlotSupplier"/> slot suppliers.
     /// </summary>
     public interface ISlotSupplier
     {
