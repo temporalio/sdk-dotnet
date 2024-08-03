13e8650bf24c12993c9328b63bbd2dd2b9f2678e
Spencer Judge <spencer@temporal.io>
2024-08-02 17:40:55 -0700

    Allow updating search attributes from schedule updates

--- src/Temporalio/Client/Schedules/ScheduleUpdate.cs
+++ src/Temporalio/Client/Schedules/ScheduleUpdate.cs
@@ -1,8 +1,25 @@
+using Temporalio.Common;
+
 namespace Temporalio.Client.Schedules
 {
     /// <summary>
     /// An update returned from an updater.
     /// </summary>
     /// <param name="Schedule">Schedule to update.</param>
-    public record ScheduleUpdate(Schedule Schedule);
+    /// <param name="SearchAttributes">Optional indexed attributes that can be used while querying
+    /// schedules via the list schedules APIs. The key and value type must be registered with
+    /// Temporal server.
+    ///
+    /// <ul>
+    /// <li>If null, the search attributes will not be updated.</li>
+    /// <li>If present but empty, the search attributes will be cleared.</li>
+    /// <li>If present and non-empty, all search attributes will be updated to the provided
+    ///     collection. This means existing attributes which do not exist in the provided collection
+    ///     will be wiped out. You can copy attributes from the existing <see
+    ///     cref="ScheduleDescription"/> to avoid this.</li>
+    /// </ul>
+    /// </param>
+    public record ScheduleUpdate(
+        Schedule Schedule,
+        SearchAttributeCollection? SearchAttributes = null);
 }
--- src/Temporalio/Client/TemporalClient.Schedules.cs
+++ src/Temporalio/Client/TemporalClient.Schedules.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf;
 using Temporalio.Api.Enums.V1;
 using Temporalio.Api.WorkflowService.V1;
 using Temporalio.Client.Interceptors;
@@ -203,6 +204,7 @@ namespace Temporalio.Client
                         Schedule = await update.Schedule.ToProtoAsync(Client.Options.DataConverter).ConfigureAwait(false),
                         Identity = Client.Connection.Options.Identity,
                         RequestId = Guid.NewGuid().ToString(),
+                        SearchAttributes = update.SearchAttributes?.ToProto(),
                     },
                     DefaultRetryOptions(input.RpcOptions)).ConfigureAwait(false);
             }
