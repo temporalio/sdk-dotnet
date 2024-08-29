b58c066d7b28844f9164387f52df9de7a1a71c44
Chad Retz <chad@temporal.io>
2024-08-29 09:40:35 -0500

    Fix cloud CI run and minor doc fix
    Fixes #331
    Fixes #333

--- src/Temporalio/Client/WorkflowCancelOptions.cs
+++ src/Temporalio/Client/WorkflowCancelOptions.cs
@@ -8,7 +8,7 @@ namespace Temporalio.Client
     public class WorkflowCancelOptions : ICloneable
     {
         /// <summary>
-        /// Gets or sets RPC options for starting the workflow.
+        /// Gets or sets RPC options for cancelling the workflow.
         /// </summary>
         public RpcOptions? Rpc { get; set; }
 
