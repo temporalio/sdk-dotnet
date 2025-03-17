1accb138691bcb9fbd6f35effe68dcaf1f6acbca
Chad Retz <chad@temporal.io>
2025-03-17 14:56:33 -0500

    Apply modern event loop algorithm with new SDK flag
    Fixes #427

--- src/Temporalio/Worker/WorkflowLogicFlag.cs
+++ src/Temporalio/Worker/WorkflowLogicFlag.cs
@@ -11,5 +11,21 @@ namespace Temporalio.Worker
         /// set.
         /// </summary>
         ReorderWorkflowCompletion = 1,
+
+        /// <summary>
+        /// Before this flag, workflow async logic did the following (in no particular order):
+        /// * Reorder jobs itself.
+        /// * Schedule primary workflow method when the initialize job was seen.
+        /// * Tick the event loop after each job processed.
+        /// * Check all conditions at once and resolve all that are satisfied.
+        /// We have since learned that all of these are wrong. So if this flag is set on the first
+        /// activation/task of a workflow, the following logic now applies respectively:
+        /// * Leave Core's job ordering as is.
+        /// * Do not schedule the primary workflow method until after all jobs have been applied and
+        ///   it's not already present.
+        /// * Tick the event loop only once after everything applied.
+        /// * Only resolve the first condition that is satisfied and re-run the entire event loop.
+        /// </summary>
+        ApplyModernEventLoopLogic = 2,
     }
 }
