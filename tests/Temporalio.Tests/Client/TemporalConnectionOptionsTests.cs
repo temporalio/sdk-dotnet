30b8aa393794747aa3e88eb1fae88569db67f176
Thomas Hardy <thomas.hardy@temporal.io>
2025-12-02 18:12:57 -0800

    add a couple tests, use notnull assertion

--- tests/Temporalio.Tests/Client/TemporalConnectionOptionsTests.cs
+++ tests/Temporalio.Tests/Client/TemporalConnectionOptionsTests.cs
@@ -19,7 +19,7 @@ public unsafe class TemporalConnectionOptionsTests
         var interopOptions = options.ToInteropOptions(scope);
 
         // TLS should be auto-enabled when API key is provided and TLS not explicitly set
-        Assert.True(interopOptions.tls_options != null);
+        Assert.NotNull(interopOptions.tls_options);
     }
 
     [Fact]
@@ -36,6 +36,34 @@ public unsafe class TemporalConnectionOptionsTests
         var interopOptions = options.ToInteropOptions(scope);
 
         // TLS should remain disabled when explicitly set to null
-        Assert.True(interopOptions.tls_options == null);
+        Assert.NotNull(interopOptions.tls_options);
+    }
+
+    [Fact]
+    public void ToInteropOptions_TlsDisabled_WhenNoApiKeyAndTlsNotSet()
+    {
+        var options = new TemporalConnectionOptions("localhost:7233");
+
+        using var scope = new Scope();
+        var interopOptions = options.ToInteropOptions(scope);
+
+        // TLS should be disabled when no API key and TLS not set
+        Assert.Null(interopOptions.tls_options);
+    }
+
+    [Fact]
+    public void ToInteropOptions_TlsEnabled_WhenExplicitlySet()
+    {
+        var options = new TemporalConnectionOptions("localhost:7233")
+        {
+            Identity = "test-identity",
+            Tls = new TlsOptions(),
+        };
+
+        using var scope = new Scope();
+        var interopOptions = options.ToInteropOptions(scope);
+
+        // TLS should be enabled when explicitly set
+        Assert.NotNull(interopOptions.tls_options);
     }
 }
