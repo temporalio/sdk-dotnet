69b33fa5e1c5dc20656bff6411ef2e5f102d37ec
Quinn Klassen <klassenq@gmail.com>
2026-06-12 10:05:40 -0700

    Add Standalone Nexus Operation links

---
+++ tests/Temporalio.Tests/Nexus/ProtoLinkExtensionsTests.cs
@@ -0,0 +1,97 @@
+namespace Temporalio.Tests.Nexus;
+
+using NexusRpc;
+using Temporalio.Nexus;
+using Xunit;
+
+public class ProtoLinkExtensionsTests
+{
+    [Fact]
+    public void NexusOperation_ToNexusLink_RoundTrips()
+    {
+        var nexusOp = new Api.Common.V1.Link.Types.NexusOperation
+        {
+            Namespace = "my-namespace",
+            OperationId = "my-op-id",
+            RunId = "my-run-id",
+        };
+
+        var nexusLink = nexusOp.ToNexusLink();
+        Assert.Equal(
+            Api.Common.V1.Link.Types.NexusOperation.Descriptor.FullName,
+            nexusLink.Type);
+
+        var roundTripped = nexusLink.ToNexusOperationLink();
+        Assert.Equal(nexusOp.Namespace, roundTripped.Namespace);
+        Assert.Equal(nexusOp.OperationId, roundTripped.OperationId);
+        Assert.Equal(nexusOp.RunId, roundTripped.RunId);
+    }
+
+    [Fact]
+    public void NexusOperation_ToNexusLink_SpecialCharactersRoundTrip()
+    {
+        var nexusOp = new Api.Common.V1.Link.Types.NexusOperation
+        {
+            Namespace = "ns/with spaces",
+            OperationId = "op?id=1&foo=bar",
+            RunId = "run+id",
+        };
+
+        var nexusLink = nexusOp.ToNexusLink();
+        var roundTripped = nexusLink.ToNexusOperationLink();
+        Assert.Equal(nexusOp.Namespace, roundTripped.Namespace);
+        Assert.Equal(nexusOp.OperationId, roundTripped.OperationId);
+        Assert.Equal(nexusOp.RunId, roundTripped.RunId);
+    }
+
+    [Fact]
+    public void ToNexusOperationLink_RejectsNonTemporalScheme()
+    {
+        var link = new NexusLink(
+            new Uri("https://somehost/namespaces/ns/nexus-operations/op/run/details"),
+            Api.Common.V1.Link.Types.NexusOperation.Descriptor.FullName);
+        Assert.Throws<ArgumentException>(() => link.ToNexusOperationLink());
+    }
+
+    [Fact]
+    public void ToNexusOperationLink_RejectsUnexpectedHost()
+    {
+        var link = new NexusLink(
+            new Uri("temporal://somehost/namespaces/ns/nexus-operations/op/run/details"),
+            Api.Common.V1.Link.Types.NexusOperation.Descriptor.FullName);
+        Assert.Throws<ArgumentException>(() => link.ToNexusOperationLink());
+    }
+
+    [Fact]
+    public void ToNexusOperationLink_RejectsInvalidPath()
+    {
+        var link = new NexusLink(
+            new Uri("temporal:///namespaces/ns/workflows/wf/run/history"),
+            Api.Common.V1.Link.Types.NexusOperation.Descriptor.FullName);
+        Assert.Throws<ArgumentException>(() => link.ToNexusOperationLink());
+    }
+
+    [Fact]
+    public void WorkflowEvent_ToNexusLink_RoundTrips()
+    {
+        var wfEvent = new Api.Common.V1.Link.Types.WorkflowEvent
+        {
+            Namespace = "my-namespace",
+            WorkflowId = "my-wf",
+            RunId = "my-run-id",
+            EventRef = new() { EventId = 1, EventType = Api.Enums.V1.EventType.WorkflowExecutionStarted },
+        };
+
+        var nexusLink = wfEvent.ToNexusLink();
+        Assert.Equal(
+            Api.Common.V1.Link.Types.WorkflowEvent.Descriptor.FullName,
+            nexusLink.Type);
+
+        var roundTripped = nexusLink.ToWorkflowEvent();
+        Assert.Equal(wfEvent.Namespace, roundTripped.Namespace);
+        Assert.Equal(wfEvent.WorkflowId, roundTripped.WorkflowId);
+        Assert.Equal(wfEvent.RunId, roundTripped.RunId);
+        Assert.Equal(wfEvent.EventRef.EventId, roundTripped.EventRef.EventId);
+        Assert.Equal(wfEvent.EventRef.EventType, roundTripped.EventRef.EventType);
+    }
+}
