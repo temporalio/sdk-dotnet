8ad42e440df7332473b74aa40baac45055c62eb6
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2026-09-14 09:33:28 -0700

    chore: bump test CLI to v1.9.0-rc.1

--- tests/Temporalio.Tests/WorkflowEnvironment.cs
+++ tests/Temporalio.Tests/WorkflowEnvironment.cs
@@ -71,7 +71,7 @@ public sealed class WorkflowEnvironment : IAsyncLifetime, IAsyncDisposable
             {
                 DevServerOptions = new()
                 {
-                    DownloadVersion = "v1.8.3-server-1.32.0-162.0",
+                    DownloadVersion = "v1.9.0-rc.1",
                     ExtraArgs = new List<string>
                     {
                         // Disable search attribute cache
