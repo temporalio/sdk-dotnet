b4f6c2193c565e5627e07467af7bc3515540a61d
Justin Anderson <44687433+jmaeagle99@users.noreply.github.com>
2026-09-16 11:03:03 -0700

    fix: apply serialization context to Nexus operation summary (#894)

--- src/Temporalio/Worker/WorkflowInstance.cs
+++ src/Temporalio/Worker/WorkflowInstance.cs
@@ -2750,7 +2750,7 @@ namespace Temporalio.Worker
                 {
                     workflowCommand.UserMetadata = new()
                     {
-                        Summary = instance.payloadConverterNoContext.ToPayload(summary),
+                        Summary = payloadConverter.ToPayload(summary),
                     };
                 }
                 instance.AddCommand(workflowCommand);
